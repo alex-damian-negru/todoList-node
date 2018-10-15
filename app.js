11938cb9c3d67cbc85d3bcbcc4ef658f2ed4ab62
Alex Damian Negru <negru.alex.damian@gmail.com>
2018-10-16 00:14:25 +0300

    Add GET `v1/status` endpoint with Postman documentation

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/v1/status', (req, res) => res.status(204).send(''))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
